27c94aa104088c06e2090bc3ed22516b599efb25
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-11-13 12:18:35 -0800

    added random spell and clearing old spell

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 async function searchSpells() {
-    const response = await fetch('https://www.dnd5eapi.co/api/spells');
-    const data = await response.json();
-    const searchInput = document.getElementById('search-bar').value.toLowerCase();
-    const results = data.results.filter(spell => spell.name.toLowerCase().includes(searchInput));
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '';
-    results.forEach(async spell => {
-        const spellResponse = await fetch(`https://www.dnd5eapi.co${spell.url}`);
+        const response = await fetch('https://www.dnd5eapi.co/api/spells');
+        const data = await response.json();
+        const searchInput = document.getElementById('search-bar').value.toLowerCase();
+        const results = data.results.filter(spell => spell.name.toLowerCase().includes(searchInput));
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = '';
+        results.forEach(async spell => {
+                const spellResponse = await fetch(`https://www.dnd5eapi.co${spell.url}`);
+                const spellData = await spellResponse.json();
+                const spellDiv = document.createElement('div');
+                spellDiv.innerHTML = `<h3>${spellData.name}</h3><p>${spellData.desc}</p>`;
+                resultsDiv.appendChild(spellDiv);
+        });
+}
+
+document.addEventListener('keydown', async (event) => {
+    if (event.key === '=') {
+        const response = await fetch('https://www.dnd5eapi.co/api/spells');
+        const data = await response.json();
+        const randomSpellIndex = Math.floor(Math.random() * data.results.length);
+        const randomSpell = data.results[randomSpellIndex];
+        const spellResponse = await fetch(`https://www.dnd5eapi.co${randomSpell.url}`);
         const spellData = await spellResponse.json();
+        const resultsDiv = document.getElementById('results');
+        resultsDiv.innerHTML = ''; // clear the results div
         const spellDiv = document.createElement('div');
         spellDiv.innerHTML = `<h3>${spellData.name}</h3><p>${spellData.desc}</p>`;
         resultsDiv.appendChild(spellDiv);
-    });
+    }
+});
+});
 
-   
