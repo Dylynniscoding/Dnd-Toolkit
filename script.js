13c83fc808a3427da4c68d999a57c447ad95bb9c
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-11-13 12:35:16 -0800

    fixed random spell undefined bug

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
 async function searchSpells() {
-        const response = await fetch('https://www.dnd5eapi.co/api/spells');
-        const data = await response.json();
-        const searchInput = document.getElementById('search-bar').value.toLowerCase();
-        const results = data.results.filter(spell => spell.name.toLowerCase().includes(searchInput));
-        const resultsDiv = document.getElementById('results');
-        resultsDiv.innerHTML = '';
-        results.forEach(async spell => {
-                const spellResponse = await fetch(`https://www.dnd5eapi.co${spell.url}`);
-                const spellData = await spellResponse.json();
-                const spellDiv = document.createElement('div');
-                spellDiv.innerHTML = `<h3>${spellData.name}</h3><p>${spellData.desc}</p>`;
-                resultsDiv.appendChild(spellDiv);
-        });
+                const response = await fetch('https://www.dnd5eapi.co/api/spells');
+                const data = await response.json();
+                const searchInput = document.getElementById('search-bar').value.toLowerCase();
+                const results = data.results.filter(spell => spell.name.toLowerCase().includes(searchInput));
+                const resultsDiv = document.getElementById('results');
+                resultsDiv.innerHTML = '';
+                results.forEach(async spell => {
+                                const spellResponse = await fetch(`https://www.dnd5eapi.co${spell.url}`);
+                                const spellData = await spellResponse.json();
+                                const spellDiv = document.createElement('div');
+                                spellDiv.innerHTML = `<h3>${spellData.name}</h3><p>${spellData.desc}</p>`;
+                                resultsDiv.appendChild(spellDiv);
+                });
 }
 
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', async function(event) {
     if (event.key === '=') {
-        const randomIndex = Math.floor(Math.random() * 319);
+        const response = await fetch('https://www.dnd5eapi.co/api/spells');
+        const data = await response.json();
+        const spells = data.results;
+        const randomSpellIndex = Math.floor(Math.random() * spells.length);
+        const randomSpell = spells[randomSpellIndex];
+        const randomSpellResponse = await fetch(randomSpell.url);
+        const randomSpellData = await randomSpellResponse.json();
         const resultsDiv = document.getElementById('results');
-        resultsDiv.innerHTML = '';
-        const spellResponse = await fetch(`https://www.dnd5eapi.co/api/spells/${randomIndex}`);
-        const spellData = await spellResponse.json();
-        const spellDiv = document.createElement('div');
-        spellDiv.innerHTML = `<h3>${spellData.name}</h3><p>${spellData.desc}</p>`;
-        resultsDiv.appendChild(spellDiv);
+        resultsDiv.innerHTML = `<div><h3>${randomSpellData.name}</h3><p>${randomSpellData.desc}</p></div>`;
     }
 });
 
-// const resultsDiv = document.getElementById('results');
-// resultsDiv.addEventListener('DOMNodeInserted', () => {
-//     if (resultsDiv.children.length > 1) {
-//         resultsDiv.removeChild(resultsDiv.firstChild);
-//     }
-// });
 
 
-    
-}
+
+
+
